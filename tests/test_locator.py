3a65b2ee3c519c0111ed43cb46d6820e4f3f4ee1
matyasmagera <94050118+matyasmagera@users.noreply.github.com>
2025-04-10 08:34:24 +0200

    hypertext_lokator

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -11,5 +11,13 @@ def test_page_title():
         page = browser.new_page()
         page.goto(f"file:///{cesta}")
         nadpis_1 = page.locator('h1').first
+        nadpis_text = page.locator('text="Nadpis 1"')
+        div_1 = page.locator('.container')
+        odkaz = page.locator('a')
+
+        expect(odkaz).to_be_visible()
         expect(nadpis_1).to_be_visible()
+        expect(nadpis_text).to_be_visible()
+        expect(div_1).to_be_visible()
+        
         browser.close()
